8874e696eba32aa8f3f05f3b083de797c4ed71d9
muktiwrtm <50369374+muktiwrtm@users.noreply.github.com>
2020-02-20 12:23:21 +0700

    Add files via upload

---
+++ matlabke3/pert4.m
@@ -0,0 +1,38 @@
+clc
+clear
+
+%x= 1:15
+%y = x.^2 
+%z= x.^3
+
+
+%title('y= x^3 + 2')
+%xlabel('x')
+%ylabel('y')
+%text(10,1002, 'andi')
+
+%gtext('hehehe')
+%figure(2)
+%plot(x)
+%subplot(2,1,1)
+%plot(x)
+%subplot(2,1,1)
+%plot(y)
+
+%plot3(x,y,z)
+%hold on
+%plot3(y,z,x)
+%hold on 
+%plot3(z,x,y)
+
+ %t=1:0.01:25; 
+% X=sin(t); Y=cos(t); Z=0.5*t; 
+% plot3(X,Y,Z) 
+% xlabel('sumbu X'); ylabel('sumbu Y'); 
+% zlabel('sumbu Z'); 
+% title ('Helix');
+
+
+
+
+
